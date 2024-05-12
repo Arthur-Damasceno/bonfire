2878db87fec1d38ddcda23f8d9dd7e17e4008e7a
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-05-12 17:18:19 -0300

    feat: get and delete requests

--- src/protocol.rs
+++ src/protocol.rs
@@ -3,12 +3,16 @@ use tokio::{io::AsyncReadExt, net::TcpStream};
 #[derive(Debug, Clone)]
 pub enum Request {
     Ping,
+    Get(Vec<u8>),
     Insert(Vec<u8>, Vec<u8>),
+    Delete(Vec<u8>)
 }
 
 impl Request {
     pub const PING: u8 = 0;
+    pub const GET: u8 = 1;
     pub const INSERT: u8 = 2;
+    pub const DELETE: u8 = 3;
 }
 
 pub struct Connection {
@@ -25,6 +29,14 @@ impl Connection {
 
         match kind {
             Request::PING => Ok(Request::Ping),
+            Request::GET => {
+                let len = self.stream.read_u16().await? as usize;
+                let mut key = vec![0; len];
+
+                self.stream.read_exact(&mut key).await?;
+
+                Ok(Request::Get(key))
+            }
             Request::INSERT => {
                 let key_len = self.stream.read_u16().await? as usize;
                 let value_len = self.stream.read_u32().await? as usize;
@@ -36,6 +48,14 @@ impl Connection {
                 self.stream.read_exact(&mut value).await?;
 
                 Ok(Request::Insert(key, value))
+            }
+            Request::DELETE => {
+                let len = self.stream.read_u16().await? as usize;
+                let mut key = vec![0; len];
+
+                self.stream.read_exact(&mut key).await?;
+
+                Ok(Request::Delete(key))
             }
             _ => todo!(),
         }
