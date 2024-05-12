ca1ee06e15c72c7cd1e19ae918e99185ea55297f
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-05-12 16:50:22 -0300

    feat: database

---
+++ src/database.rs
@@ -0,0 +1,23 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
+
+type Map = HashMap<Vec<u8>, Vec<u8>>;
+
+#[derive(Clone, Default)]
+pub struct Database(Arc<RwLock<Map>>);
+
+impl Database {
+    pub fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
+        self.0.read().unwrap().get(key).map(|x| x.clone())
+    }
+
+    pub fn insert(&self, key: Vec<u8>, value: Vec<u8>) {
+        self.0.write().unwrap().insert(key, value);
+    }
+
+    pub fn delete(&self, key: &[u8]) -> bool {
+        self.0.write().unwrap().remove(key).is_some()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod database;
 mod protocol;
 mod server;
 
