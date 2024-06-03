e33c3fb2db2765d406eac6cef4f00b1418b1a378
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-06-03 07:53:14 -0300

    feat: cli subcommands

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,66 @@ mod error;
 mod protocol;
 mod server;
 
-use std::{env::var, io::stdin, process::exit};
+use std::{
+    io::stdin,
+    net::{IpAddr, Ipv6Addr, SocketAddr},
+    process::exit,
+};
 
-use tokio::{select, task::spawn_blocking};
+use {
+    clap::Parser,
+    tokio::{select, task::spawn_blocking},
+};
 
 use {client::Client, error::Result, protocol::Request, server::Server};
 
-const ADDR: &str = "127.0.0.1:6530";
+const DEFAULT_SOCKET_ADDRESS: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 6530);
+
+#[derive(Debug, Parser)]
+#[command(version, about)]
+enum Args {
+    /// Start a server.
+    Server {
+        /// The address to bind to.
+        #[arg(short, long, default_value_t = DEFAULT_SOCKET_ADDRESS)]
+        bind: SocketAddr,
+    },
+    /// Connect to a server.
+    Client {
+        /// The address to connect to.
+        #[arg(short, long, default_value_t = DEFAULT_SOCKET_ADDRESS)]
+        connect: SocketAddr,
+    },
+}
 
 #[tokio::main]
 async fn main() {
-    let service = var("SERVICE").unwrap_or_else(|_| "SERVER".into());
+    let args = Args::parse();
 
-    if let Err(err) = match service.as_str() {
-        "CLIENT" | "C" => client().await,
-        "SERVER" | "S" => server().await,
-        _ => panic!("Service {service} does not exists"),
+    if let Err(err) = match args {
+        Args::Client { connect } => client(connect).await,
+        Args::Server { bind } => server(bind).await,
     } {
         eprintln!("{err}");
         exit(1);
     }
 }
 
-async fn server() -> Result {
-    let server = Server::bind(ADDR).await?;
+async fn server(addr: SocketAddr) -> Result {
+    let server = Server::bind(addr).await?;
 
     server.start().await
 }
 
-async fn client() -> Result {
-    let mut client = Client::connect(ADDR).await?;
+async fn client(addr: SocketAddr) -> Result {
+    let mut client = Client::connect(addr).await?;
 
     loop {
         select! {
             response = client.recv() => {
                 println!("{:?}", response?);
             }
-            request = spawn_blocking(|| {
-                let mut data = String::new();
-                stdin().read_line(&mut data).unwrap();
-                data
-            }) => {
+            request = spawn_blocking(read_line) => {
                 if let Ok(request) = Request::try_from(request.unwrap().as_str()) {
                     let response = client.request(&request).await?;
                     println!("{response:?}");
@@ -54,4 +73,10 @@ async fn client() -> Result {
             }
         }
     }
+}
+
+fn read_line() -> String {
+    let mut data = String::new();
+    stdin().read_line(&mut data).unwrap();
+    data
 }
